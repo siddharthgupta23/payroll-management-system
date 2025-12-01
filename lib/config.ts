768d9bb75e543ba6f5c12a069388f49b5d8a4809
siddharthgupta23 <siddharthgupta1603@gmail.com>
2025-12-01 21:50:17 +0530

    inital commit

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://payroll-management-system-fq33.onrender.com/api"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://payroll-management-system-0ok5.onrender.com/api"
