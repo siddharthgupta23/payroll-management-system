1a2f4300d4fad0923fe0675b53032b1a643dd0c7
siddharthgupta23 <siddharthgupta1603@gmail.com>
2025-12-01 10:51:06 +0530

    initial commit

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://payroll-management-systemmmmm.vercel.app/api"
