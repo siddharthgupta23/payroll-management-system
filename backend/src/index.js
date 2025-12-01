3a1ce84fac2a025a6c35e398a1fe9c4cd2f1a427
siddharthgupta23 <siddharthgupta1603@gmail.com>
2025-12-01 10:35:04 +0530

    inital commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,13 @@ app.use("/api/admin", adminRoutes)
 app.use("/api/employee", employeeRoutes)
 
 const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
