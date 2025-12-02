1859e6d51a7014c466ce53e2eb444275abecef41
siddharthgupta23 <siddharthgupta1603@gmail.com>
2025-12-02 21:07:47 +0530

    initial commit

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://localhost:3000/api"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://localhost:5000/api"
